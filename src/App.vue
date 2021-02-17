4f441730fbef2d1e9137b480cbf2093fee1093b1
Dmitriy Smirnov <dima-smirnov1@mail.ru>
2021-02-17 17:53:41 +0300

    Update with /dist

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,28 @@
     <v-main>
       <v-container>
         <v-row>
-          <router-view />
+          <v-col cols="12" class="home">
+            <v-row justify="center">
+              <v-col cols="10">
+                <records-form />
+              </v-col>
+              <v-col cols="10">
+                <records-table />
+              </v-col>
+            </v-row>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
-<style lang="scss"></style>
+<script>
+import RecordsTable from "@/components/RecordsTable";
+import RecordsForm from "@/components/RecordsForm";
+
+export default {
+  name: "App",
+  components: { RecordsForm, RecordsTable }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,5 @@ module.exports = {
     }
   },
   runtimeCompiler: true,
-  publicPath: "/"
+  publicPath: ""
 };
